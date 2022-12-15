1bfe536fee6011d42eaeb4fad426787c7b85da68
mark-todd <markpeter.todd@hotmail.co.uk>
2022-12-15 17:29:29 +0000

    Reverted to previous form

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pytest-trust-random",
-    packages=["pytest_trust_random"],
-    entry_points={"pytest11": ["trustrandom = pytest_trust_random.plugin"]},
+    entry_points={"pytest11": ["trustrandom = pytest_trust_random"]},
     classifiers=["Framework :: Pytest"],
+    version="0.1.0",
 )
