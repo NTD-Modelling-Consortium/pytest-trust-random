3711ff05fe3dfb8ebb9c3212f94dab39799b9bad
mark-todd <markpeter.todd@hotmail.co.uk>
2022-12-15 15:06:38 +0000

    Name change in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name="pytest-trust-random",
     packages=["pytest_trust_random"],
-    entry_points={"pytest11": ["pytest_trust_random = pytest_trust_random"]},
+    entry_points={"pytest11": ["trustrandom = pytest_trust_random"]},
     classifiers=["Framework :: Pytest"],
 )
