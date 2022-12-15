e8f3be6481f9c3f4033bea7011f3751648cab81d
mark-todd <markpeter.todd@hotmail.co.uk>
2022-12-15 16:29:22 +0000

    Added new plugin style

--- setup.py
+++ setup.py
@@ -1,8 +1,34 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup
 
-setup(
-    name="pytest-trust-random",
-    packages=["pytest_trust_random"],
-    entry_points={"pytest11": ["trustrandom = pytest_trust_random"]},
-    classifiers=["Framework :: Pytest"],
-)
+packages = ["pytest_trust_random"]
+
+package_data = {"": ["*"]}
+
+install_requires = [
+    "joblib>=1.2.0,<2.0.0",
+    "numpy>=1.23.5,<2.0.0",
+    "pydantic>=1.10.2,<2.0.0",
+    "pytest-rerunfailures>=10.3,<11.0",
+    "pytest>=7.2.0,<8.0.0",
+]
+
+setup_kwargs = {
+    "name": "pytest-trust-random",
+    "version": "0.1.0",
+    "description": "",
+    "long_description": None,
+    "author": "mark-todd",
+    "author_email": "markpeter.todd@hotmail.co.uk",
+    "maintainer": None,
+    "maintainer_email": None,
+    "url": None,
+    "packages": packages,
+    "package_data": package_data,
+    "install_requires": install_requires,
+    "python_requires": ">=3.10,<3.11",
+    "entry_points": {"pytest11": ["trustrandom = pytest_trust_random.plugin"]},
+    "classifiers": ["Framework :: Pytest"],
+}
+
+setup(**setup_kwargs)
