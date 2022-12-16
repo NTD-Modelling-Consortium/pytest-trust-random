c2f19fc448fc7297d9999e27d379beef3d9fdf12
mark-todd <markpeter.todd@hotmail.co.uk>
2022-12-16 10:04:39 +0000

    Made key name of plugin

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup
 
 setup(
     name="pytest-trust-random",
-    entry_points={"pytest11": ["trustrandom = pytest_trust_random"]},
+    entry_points={"pytest11": ["name_of_plugin = pytest_trust_random"]},
     classifiers=["Framework :: Pytest"],
     version="0.1.0",
+    packages=["pytest_trust_random"],
 )
