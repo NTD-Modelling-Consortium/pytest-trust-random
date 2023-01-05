4112aa750a25885d19e76d0680f7c63d041e6c7c
mark-todd <markpeter.todd@hotmail.co.uk>
2023-01-05 16:42:07 +0000

    Moved calc failure to new file

--- pytest_trust_random/__init__.py
+++ pytest_trust_random/__init__.py
@@ -52,11 +52,9 @@ __all__ = [
 import re
 import sys
 from collections import defaultdict
-from dataclasses import dataclass
 from importlib.util import module_from_spec, spec_from_file_location
 from inspect import getmembers, isfunction
 from pathlib import Path
-from statistics import NormalDist
 from typing import Callable, Iterator, Type
 
 import pytest
@@ -67,6 +65,7 @@ from .auto_benchmarker import (
     BaseTestModel,
     TrustRandomConfig,
 )
+from .calc_failure import FailureProbabilities, calc_failure_prob
 
 FILE_NAME_PATTERN = re.compile(r"benchmark_test_.+.py")
 
@@ -218,65 +217,4 @@ def pytest_addoption(parser):
         dest="genbenchmark",
         action="store_true",
         help="Should (re)generate benchmark?",
-    )
-
-
-@dataclass
-class FailureProbabilities:
-    per_test_no_reruns: float
-    per_test_reruns: float
-    one_test_from_all_no_reruns: float
-    one_test_from_all_reruns: float
-
-
-def calc_failure_prob(
-    acceptable_st_devs: float,
-    re_runs: int,
-    independent_variables: int,
-    n_tests: int,
-    verbose: bool = True,
-) -> FailureProbabilities:
-    """
-    Calculates and prints the probability of failure on any particular run of the
-    test suite.
-
-    Args:
-        acceptable_st_devs (float): The number of standard devs within which a test will pass
-        re_runs (int): The number of times a test is allowed to fail.
-        independent_variables (int): The number of fully independent variable in the problem.
-        May need to be determined experimentally, by measuring real rate of failure.
-        n_tests (int): The total number of tests.
-    """
-    st_devs = acceptable_st_devs
-    re_runs = re_runs
-    independent_variables = independent_variables
-    st_dev_prob = NormalDist().cdf(-st_devs)
-    success_prob = 1 - st_dev_prob * 2
-    prob_of_one_test_fail = 1 - success_prob**independent_variables
-
-    prob_of_failing_all_re_runs = prob_of_one_test_fail ** (re_runs + 1)
-
-    prob_of_one_of_all_tests_failing = 1 - (1 - prob_of_one_test_fail) ** n_tests
-
-    prob_of_one_of_all_tests_failing_reruns = (
-        1 - (1 - prob_of_failing_all_re_runs) ** n_tests
-    )
-    if verbose:
-        print("Fail probability per test (assuming no reruns): ", prob_of_one_test_fail)
-        print(
-            "Fail probability per test (assuming reruns): ", prob_of_failing_all_re_runs
-        )
-        print(
-            "Probability of one test failing: (assuming no reruns)",
-            prob_of_one_of_all_tests_failing,
-        )
-        print(
-            "Probability of one test failing: (assuming reruns)",
-            prob_of_one_of_all_tests_failing_reruns,
-        )
-    return FailureProbabilities(
-        per_test_no_reruns=prob_of_one_test_fail,
-        per_test_reruns=prob_of_failing_all_re_runs,
-        one_test_from_all_no_reruns=prob_of_one_of_all_tests_failing,
-        one_test_from_all_reruns=prob_of_one_of_all_tests_failing_reruns,
     )
